33fbf3df2c51fe649305d5ed4303793f7668f104
Bisrat Tamire <biarattamire288@gmail.com>
2024-08-29 13:17:03 +0300

    typical

--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-
+import Typical from "react-typical";
 export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="#">
-              <i className="fa fa-facebook-square"></i>
+            <a href="https://www.linkedin.com/in/bisrat-tamire-24737928b/">
+              <i className="fa fa-linkedin-square"></i>
             </a>
 
-            <a href="#">
-              <i className="fa fa-google-plus-square"></i>
+            <a href="https://github.com/Bisrate19">
+              <i className="fa fa-github-square"></i>
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/kidus_tamire/">
               <i className="fa fa-instagram-square"></i>
             </a>
 
-            <a href="#">
+            <a href="https://www.youtube.com/channel/UC8crG29DqXeeLXCWM9q6bCg">
               <i className="fa fa-youtube-square"></i>
             </a>
 
@@ -25,6 +25,34 @@ export default function Profile() {
               <i className="fa fa-twitter-square"></i>
             </a>
           </div>
+        </div>
+        <div className="profile-details-name">
+          <span className="" primary-text>
+            {""}
+            Hello, i'm <span className="highlighted-text">Bisrat</span>
+          </span>
+        </div>
+        <div className="profile-details-role">
+          <span className="primary-text">
+            {""}
+            <h1>
+              <Typical
+                loop={Infinity}
+                steps={[
+                  "Enthusiastic Dev",
+                  1000,
+                  "full Stack Developer",
+                  1000,
+                  "MERN Stack Dev",
+                  1000,
+                  "Cross Platform V",
+                  1000,
+                  "React/Next Dev",
+                  1000,
+                ]}
+              />
+            </h1>
+          </span>
         </div>
       </div>
     </div>
