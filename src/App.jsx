81be3da2b74182544e38585ca81f04e720aa8b50
Bisrat Tamire <biarattamire288@gmail.com>
2024-08-29 08:20:17 +0300

    icons,bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import Profile from "./PortfolioContainer/Home/Profile";
 function App() {
-    return <div className="App"></div>
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
 }
 export default App;
