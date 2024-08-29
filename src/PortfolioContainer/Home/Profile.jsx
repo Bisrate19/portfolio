8e061e0e17068e88158368aeef24366b2240be74
Bisrat Tamire <biarattamire288@gmail.com>
2024-08-29 17:31:14 +0300

    modify+profile pic

--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -6,24 +6,26 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.linkedin.com/in/bisrat-tamire-24737928b/">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/bisrat-tamire-24737928b/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
 
-            <a href="https://github.com/Bisrate19">
-              <i className="fa fa-github-square"></i>
-            </a>
-            <a href="https://www.instagram.com/kidus_tamire/">
-              <i className="fa fa-instagram-square"></i>
-            </a>
+              <a href="https://github.com/Bisrate19">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://www.instagram.com/kidus_tamire/">
+                <i className="fa fa-instagram-square"></i>
+              </a>
 
-            <a href="https://www.youtube.com/channel/UC8crG29DqXeeLXCWM9q6bCg">
-              <i className="fa fa-youtube-square"></i>
-            </a>
+              <a href="https://www.youtube.com/channel/UC8crG29DqXeeLXCWM9q6bCg">
+                <i className="fa fa-youtube-square"></i>
+              </a>
 
-            <a href="#">
-              <i className="fa fa-twitter-square"></i>
-            </a>
+              <a href="#">
+                <i className="fa fa-twitter-square"></i>
+              </a>
+            </div>
           </div>
         </div>
         <div className="profile-details-name">
@@ -62,13 +64,13 @@ export default function Profile() {
             {""}
             Hire Me{" "}
           </button>
-          <a href="myy.pdf" download='myy.pdf'>
-            <button className="btn highlighted-btn"> Get Resume
-
-            </button>
-            
+          <a href="myy.pdf" download="myy.pdf">
+            <button className="btn highlighted-btn"> Get Resume</button>
           </a>
         </div>
+      </div>
+      <div className="profile-picture">
+        <div className="profile-picture-background"></div>
       </div>
     </div>
   );
