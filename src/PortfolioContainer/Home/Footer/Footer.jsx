f68b477cddd8bccd3f4314013b61cce4d0f24279
Bisrat Tamire <biarattamire288@gmail.com>
2024-09-01 14:14:21 +0300

    footer 2nd

--- src/PortfolioContainer/Home/Footer/Footer.jsx
+++ src/PortfolioContainer/Home/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Footer.css";
+import shapeBg from "../../../assets/Home/shape-bg.png"; // Using import instead of require
 
 export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-parent">
-        {/* <img src={require('../../../assets/home/curve-bg.png')} alt="no internet connection" /> */}
+        <img src={shapeBg} alt="no internet connection" />
       </div>
     </div>
   );
