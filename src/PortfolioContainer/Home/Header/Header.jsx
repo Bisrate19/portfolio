281bc080829b6528d4797f9a418993ecaf3285f5
Bisrat Tamire <biarattamire288@gmail.com>
2024-09-02 23:02:00 +0300

    update screen

--- src/PortfolioContainer/Home/Header/Header.jsx
+++ src/PortfolioContainer/Home/Header/Header.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-export default function Header() {
-  return ( <div>
+import React, { useState } from "react";
+import {
+  TOTAL_SCREENS,
+  GET_SCREEN_INDEX,
+} from "../../../Utilities/CommonUtils";
+import ScrollService from "../../../Utilities/ScrollScervice";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
 
-  </div>
-  )
+export default function Header() {
+  const [selectedScreen, setselectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeaderOptions] = useState(false);
+
+  const updateCurrentScreen = (updateCurrentScreen) => {
+    if (!currentScreen || !currentScreen.screenInView) return;
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+    if (screenIndex < 0) return;
+  };
+
+ 
+
+let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+
+  <div></div>;
 }
