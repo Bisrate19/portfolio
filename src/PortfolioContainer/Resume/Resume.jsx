d956731f9b207d6da18820924461388de58be228
Bisrat Tamire <biarattamire288@gmail.com>
2024-09-06 23:36:56 +0300

    staric data 3

--- src/PortfolioContainer/Resume/Resume.jsx
+++ src/PortfolioContainer/Resume/Resume.jsx
@@ -47,26 +47,88 @@ export default function Resume(props) {
     { skill: "HTML", ratingPercentage: 95 },
     { skill: "CSS", ratingPercentage: 95 },
   ];
-const projectDetails =[
-{
-    title: "personal portfolio website",
-    duration: {fromDate: "2023", toDate: "2023"},
-    description: "A personal portfolio website to showcase all details and projects at one place",
-    subHeading: "Technoogy used : React JS, Bootstrap",
-},
-{
-    title: "Final Year Project website",
-    duration: {fromDate: "2023", toDate: "2023"},
-    description: "House Rental web app to Hossaena Town",
-    subHeading: "Technoogy used : HTML,CSS,JS,SQL",
-},
-{
-    title: "personal portfolio website",
-    duration: {fromDate: "2023", toDate: "2023"},
-    description: "A personal portfolio website to showcase all details and projects at one place",
-    subHeading: "Technoogy used : React JS, Bootstrap",
-}
-]
+  const projectDetails = [
+    {
+      title: "personal portfolio website",
+      duration: { fromDate: "2023", toDate: "2023" },
+      description:
+        "A personal portfolio website to showcase all details and projects at one place",
+      subHeading: "Technoogy used : React JS, Bootstrap",
+    },
+    {
+      title: "Final Year Project website",
+      duration: { fromDate: "2023", toDate: "2023" },
+      description: "House Rental web app to Hossaena Town",
+      subHeading: "Technoogy used : HTML,CSS,JS,SQL",
+    },
+    {
+      title: "personal portfolio website",
+      duration: { fromDate: "2023", toDate: "2023" },
+      description:
+        "A personal portfolio website to showcase all details and projects at one place",
+      subHeading: "Technoogy used : React JS, Bootstrap",
+    },
+  ];
+
+  const resumeDetails = [
+    <div className="resume-screen-container" key="education">
+      <ResumeHeading
+        heading={"Wachemo University,Hossaena Ethiopia"}
+        subHeading={"Bachelor Of Science Information Technology"}
+        fromDate={"2019"}
+        toDate={"2023"}
+      />
+    </div>,
+    <div className="resume-screen-container" key="education">
+      <ResumeHeading
+        heading={"Wachemo University,Hossaena Ethiopia"}
+        subHeading={"Bachelor Of Science Information Technology"}
+        fromDate={"2019"}
+        toDate={"2023"}
+      />
+      ,
+      <div className="resume-screen-container" key="education">
+        <ResumeHeading
+          heading={"Wachemo University,Hossaena Ethiopia"}
+          subHeading={"Bachelor Of Science Information Technology"}
+          fromDate={"2019"}
+          toDate={"2023"}
+        />
+      </div>
+    </div>,
+    <div className="resume-screen-container" key="work-experience">
+      <ResumeHeading
+        heading={"Tamrat Zemarianm Wholesale Trade"}
+        subHeading={"Data Encoder/ICT Specialist"}
+        fromDate={"2023"}
+        toDate={"present"}
+      />
+      <div className="experience-description">
+        <span className="resume-description-text">
+          currently working as a data encoder ans ICT specialist for a private
+          conpany
+        </span>
+      </div>
+
+      <div className="experience-description">
+        <span className="resume-description-text">
+          developed a website for client with landing page ........
+        </span>
+        <br />
+        <span className="resume-description-text">
+          -- Integrated the web app with backend services t create a new user
+          onboarding application with dynamic form content.
+        </span>
+        <br />
+        <span className="resume-description-text">
+          -- i stretch my mental capacity to develop ui as per the given
+          designs.
+        </span>
+        <br />
+      </div>
+    </div>,
+  ];
+
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
